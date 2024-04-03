e0f018ae38666877de7b1bf6b27c62f0287583f6
TristanKatte <tristankatte@hotmail.com>
2024-04-03 12:48:04 +0200

    test met port 3000

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/calculator', function (request, response){
 
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 3000)
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
